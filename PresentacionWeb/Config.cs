6592fbe0104077cca0f1e032d76529c58a7cb76f
OLIVIERMEJIAS <mejiasoli93@gmail.com>
2021-12-18 13:46:51 -0600

    Inicio

---
+++ PresentacionWeb/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace PresentacionWeb
+{
+    public static class Config
+    {
+        public static string getCadConec
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ConnectionString"];
+            }
+        }
+
+        public static bool Profesor { get; set; }
+
+        public static bool Director { get; set; }
+
+        public static bool Asistente { get; set; }
+    }
+}
